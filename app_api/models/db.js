e6a9df3e4c6ece6885bae0b1c407f2abfc117b83
RevoideN <talhakucukoner@hotmail.com>
2022-11-26 17:47:32 +0300

    26 kasım 2022

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 require("./mekansema");
-//var dbURI = "mongodb://localhost/mekanbul";
-var dbURI = 'mongodb+srv://talha:talha123@mekanbul.swmuuqz.mongodb.net/mekanbul?retryWrites=true&w=majority';
+var dbURI = "mongodb://localhost/mekanbul";
+//var dbURI = 'mongodb+srv://talha:talha123@mekanbul.swmuuqz.mongodb.net/mekanbul?retryWrites=true&w=majority';
 
 mongoose.connect(dbURI);
 function kapat(msg,callback){
